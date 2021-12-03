ef7438fe79eed26a43801b3057d95cd030a11ecb
Mamy Fiononana HERIHIANKINANA <hery@hightao-mg.com>
2021-12-03 14:46:23 +0300

    update alert

--- src/lib/ht-kit/alert/HTAlert.jsx
+++ src/lib/ht-kit/alert/HTAlert.jsx
@@ -1,33 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './HTAlert.scss';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
-const HTAlert = () => {
-  const [open, setOpen] = useState(true);
-  function handleClose(reason) {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  }
-
-  return (
-  <div>
-      <Snackbar
-        open={open}
-        autoHideDuration={2000}
-        onClose={handleClose}
-        variant="warning"
-        message={message}
-        action={[
-          <IconButton key="close" onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        ]}
-      />
-  </div>
-  );
-};
+const HTAlert = ({
+  onClose,
+  open,
+  message,
+  autoHideDuration,
+  onClick
+}) => <div>
+  <Snackbar
+    open={open}
+    autoHideDuration={autoHideDuration}
+    onClose={onClose}
+    message={message}
+    onClick={onClick}
+    action={[
+      <IconButton key="close" onClick={onClick}>
+        <CloseIcon />
+      </IconButton>
+    ]}
+  />
+</div>
 
 export default HTAlert;
