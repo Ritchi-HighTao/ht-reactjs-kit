2c7d3b76a2d3d39dba7926c63858727a7d9bffb9
Tchi <tchi.devica@gmail.com>
2021-12-03 18:00:34 +0100

    feat: pagination and new table

--- src/lib/ht-kit/table/HTTable.jsx
+++ src/lib/ht-kit/table/HTTable.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
 
-const HTTable = (props) => (
-  <DataGrid {...props} />
-);
+import './HTTable.scss';
+
+const HTTable = (props) => {
+  const {
+    data,
+    headRenders,
+    lineRenders,
+  } = props;
+
+  const renderHead = () => {
+    if (!headRenders) {
+      return null;
+    }
+
+    return (
+      <tr>
+        {headRenders.map((headRender, i) => (<th key={i.key}>{headRender()}</th>))}
+      </tr>
+    );
+  };
+
+  const renderCell = (lineRender, lineData) => lineRender(lineData);
+
+  const renderLine = (lineData) => lineRenders.map((lineRender, i) => (
+    <td key={i.key}>{renderCell(lineRender, lineData)}</td>
+  ));
+
+  const renderBody = () => {
+    if (!data || !lineRenders) {
+      return null;
+    }
+
+    return data.map((lineData, i) => (<tr key={i.key}>{renderLine(lineData)}</tr>));
+  };
+
+  return (
+    <table className="ht-table">
+      <thead>{renderHead()}</thead>
+      <tbody>{renderBody()}</tbody>
+    </table>
+  );
+};
 
 export default HTTable;
